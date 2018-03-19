acdba55b726c42f52703136bae0b433aa971d2c6
Janior <janior_orlando@hotmail.com>
2018-03-19 17:57:33 -0500

    Version Inicial

---
+++ test.rb
@@ -0,0 +1,10 @@
+ El volumen de la unidad D es Datos
+ El n£mero de serie del volumen es: 841F-EE8D
+
+ Directorio de D:\MakeItReal\IntegraRamas
+
+19/03/2018  17:27    <DIR>          .
+19/03/2018  17:27    <DIR>          ..
+19/03/2018  17:27                 0 test.rb
+               1 archivos              0 bytes
+               2 dirs  34.206.560.256 bytes libres
